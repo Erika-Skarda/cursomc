aed5b4608bcbafb1506110b9db9e97146234171f
Erika Skarda <erikaskarda@yahoo.com.br>
2021-06-21 17:32:36 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/erikaskarda/cursomc/CursomcApplication.java
+++ src/main/java/com/erikaskarda/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.erikaskarda.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.erikaskarda.cursomc.domain.Cidade;
 import com.erikaskarda.cursomc.domain.Cliente;
 import com.erikaskarda.cursomc.domain.Endereco;
 import com.erikaskarda.cursomc.domain.Estado;
+import com.erikaskarda.cursomc.domain.Pagamento;
+import com.erikaskarda.cursomc.domain.PagamentoComBoleto;
+import com.erikaskarda.cursomc.domain.PagamentoComCartao;
+import com.erikaskarda.cursomc.domain.Pedido;
 import com.erikaskarda.cursomc.domain.Produto;
+import com.erikaskarda.cursomc.domain.enums.EstadoPagamento;
 import com.erikaskarda.cursomc.domain.enums.TipoCliente;
 import com.erikaskarda.cursomc.repositories.CategoriaRepository;
 import com.erikaskarda.cursomc.repositories.CidadeRepository;
 import com.erikaskarda.cursomc.repositories.ClienteRepository;
 import com.erikaskarda.cursomc.repositories.EnderecoRepository;
 import com.erikaskarda.cursomc.repositories.EstadoRepository;
+import com.erikaskarda.cursomc.repositories.PagamentoRepository;
+import com.erikaskarda.cursomc.repositories.PedidoRepository;
 import com.erikaskarda.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -36,7 +44,10 @@ public class CursomcApplication implements CommandLineRunner {
 	private ClienteRepository clienteRepository;
 	@Autowired
 	private EnderecoRepository enderecoRepository;
-	
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -86,6 +97,22 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		Pedido ped1 = new Pedido(null, sdf.parse("21/06/2021 17:06"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("20/06/2021 17:00"), cli1, e2);
+		
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+		
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("21/06/2021 00:00"), null);
+		ped2.setPagamento(pagto2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+		
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
+		
 	}
 	
 }
--- src/main/java/com/erikaskarda/cursomc/domain/Estado.java
+++ src/main/java/com/erikaskarda/cursomc/domain/Estado.java
@@ -34,7 +34,6 @@ public class Estado implements Serializable {
 	}
 
 
-
 	public Integer getId() {
 		return id;
 	}
